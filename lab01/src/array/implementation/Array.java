07a9cbc863690a40f01500262490ff3839d86f4f
tarunraghav226 <tarun.raghav226@gmail.com>
2020-01-10 14:03:48 +0530

    Added array lab practical.

---
+++ lab01/src/array/implementation/Array.java
@@ -0,0 +1,4 @@
+package array.implementation;
+
+public class Array {
+}
---
+++ lab01/src/array/interfaces/ArrayInterface.java
@@ -0,0 +1,11 @@
+package array.interfaces;
+
+public interface ArrayInterface {
+    String displayArray();
+
+    int[] insert(int size);
+
+    int sum();
+
+    float average();
+}

c7c6b7760649287af12cbb64178b48cf286d631c
tarunraghav226 <tarun.raghav226@gmail.com>
2020-01-15 15:42:40 +0530

    Added optimized bubble sort code.

--- lab01/src/array/implementation/Array.java
+++ lab01/src/array/implementation/Array.java
@@ -101,14 +101,18 @@ public class Array implements ArrayInterface {
 
     @Override
     public void sortArray() {
+        boolean flag = false;
         for (int i = 0; i < this.size - 1; i++) {
             for (int j = 0; j < this.size - 1 - i; j++) {
                 if (array[j] > array[i]) {
                     int temp = array[j];
                     array[j] = array[i];
                     array[i] = temp;
+                    flag = true;
                 }
             }
+            if (flag == false)
+                return;
         }
     }
 }

6146a81a63d986a7c1d34b1c718ff026ee7c8f38
tarunraghav226 <tarun.raghav226@gmail.com>
2020-01-15 15:58:16 +0530

    Added MySort java class.

--- lab02/src/sortArray/MySort.java
+++ lab02/src/sortArray/MySort.java
@@ -1,5 +1,44 @@
 package sortArray;
 
-public class MySort {
+import interfaces.SortOperations;
 
+public class MySort implements SortOperations {
+    @Override
+    public void sortm1(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length - 1; j++) {
+                if (arr[j] > arr[j] + 1) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    @Override
+    public void sortm2(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    @Override
+    public void sortm3(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length - i - 1; j++) {
+                if (arr[j] < arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
 }
